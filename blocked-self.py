0fa3f0775fb9ce3a6503d7ebbfd770ed0bb74a44
Litoch <36871906+Litoch@users.noreply.github.com>
2020-02-01 17:27:21 +0800

    Update blocked-self.py

--- blocked-self.py
+++ blocked-self.py
@@ -5,20 +5,20 @@ with open('keywords.txt', 'w') as f:
     f.writelines(assigned_words)
 
 # read and load keywords
-
 keywords = []
 with open('keywords.txt') as f:
     lines = f.readlines()
     for line in lines:
         line = line.strip()
         keywords.append(line)
-
 print(keywords)
 
+# define filter function
 def filter(userwords = '', replacement = '*'):
     for word in keywords:
         userwords = userwords.replace(word, replacement * len(word))
     return userwords
 
+# test
 text = input('Enter your words: ')
 print(filter(text))
